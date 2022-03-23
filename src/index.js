10a8cf930ef72cccc3ac85bfc72ce79b95db5fc1
Mark <mark.n.bacon.jr@gmail.com>
2022-03-23 15:28:14 -0400

    complete!

--- src/index.js
+++ src/index.js
@@ -1,1 +1,47 @@
 console.log('%c HI', 'color: firebrick')
+window.addEventListener("DOMContentLoaded", () => {
+
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+        .then(resp => resp.json())
+        .then(data => {
+            data.message.forEach(message => {
+                const img = document.createElement('img')
+                const container = document.querySelector('#dog-image-container')
+                img.src = message
+                container.append(img)
+            }
+            )
+        })
+
+        function selectLetter(){
+            const letterSelected = document.getElementById("breed-dropdown")
+                letterSelected.addEventListener("change", (e) => {
+                    const singleLetter = e.target.value
+                    const wholeList = document.querySelectorAll("li")
+                    wholeList.forEach((li) => {
+                        if (li.innerText.charAt(0) === singleLetter){
+                            li.style.display = ""
+                        }else{
+                            li.style.display = 'none'
+                        }
+                    })
+                })
+        }
+
+    fetch('https://dog.ceo/api/breeds/list/all')
+        .then(resp => resp.json())
+        .then(data => {
+            for (const breed in data.message) {
+                const dog = document.createElement('li')
+                const list = document.querySelector('#dog-breeds')
+                dog.textContent = breed
+                list.append(dog)
+                dog.addEventListener("click", () => {
+                    dog.style.color = "blue"
+                })
+                selectLetter()
+            }
+        }
+        )
+
+    })
